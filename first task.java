2b806d70a7cfbf66812270e72c481690f0957b78
giorgi-2000-2 <157491297+giorgi-2000-2@users.noreply.github.com>
2025-12-04 16:06:08 +0400

    Update first task.java

--- first task.java
+++ first task.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-     String mainStr = "გიორგი მიქელაძე";
+        Scanner Obj = new Scanner(System.in);
+        System.out.print("შეიყვანეთ ტექსტი: ");
+        String text = Obj.nextLine();
 
-     String name = (mainStr.length()%2==0)? "ლუწია" : "კენტია";
+        String name = (text.length()%2==0)? "ლუწია" : "კენტია";
         System.out.println(name);
     }
 }
+
