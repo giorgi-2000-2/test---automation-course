e538d5e71bf1b13bc53e1ab8b58a1aad907a7bf2
giorgi-2000-2 <157491297+giorgi-2000-2@users.noreply.github.com>
2025-12-04 14:58:32 +0400

    Create first task..java

---
+++ first task..java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int [] numbers = {0,1,2,3,4,5,6,7,8,9};
+        for(int i = 0; numbers.length > i; i++){
+            if(i==5)continue;
+            System.out.println(numbers[i]);
+
+
+        }
+    }
+
+}
