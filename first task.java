b9d1ac0fac433bdf6f9233049d498b2d4dc49b74
giorgi-2000-2 <157491297+giorgi-2000-2@users.noreply.github.com>
2025-12-04 14:24:38 +0400

    Create first task.java

---
+++ first task.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+     String mainStr = "გიორგი მიქელაძე";
+
+     String name = (mainStr.length()%2==0)? "ლუწია" : "კენტია";
+        System.out.println(name);
+    }
+}
